696fe7e06d5364e84e6ac7db7a0c4f4c3d4e8b57
Jack Steam <jacksteamdev@gmail.com>
2021-06-30 11:21:53 -0500

    chore: add rollup plugin summary

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 /* eslint-env node */
 
-import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
+import {
+  chromeExtension,
+  simpleReloader,
+} from 'rollup-plugin-chrome-extension'
 
 import commonjs from '@rollup/plugin-commonjs'
 import { emptyDir } from 'rollup-plugin-empty-dir'
 import path from 'path'
 import resolve from '@rollup/plugin-node-resolve'
+import summary from 'rollup-plugin-summary'
 import typescript from '@rollup/plugin-typescript'
 import zip from 'rollup-plugin-zip'
 
@@ -16,7 +20,7 @@ export default {
   output: {
     dir: 'dist',
     format: 'esm',
-    chunkFileNames: path.join('chunks','[name]-[hash].js'),
+    chunkFileNames: path.join('chunks', '[name]-[hash].js'),
   },
   plugins: [
     chromeExtension(),
@@ -29,5 +33,6 @@ export default {
     emptyDir(),
     // Outputs a zip file in ./releases
     isProduction && zip({ dir: 'releases' }),
+    summary(),
   ],
 }
